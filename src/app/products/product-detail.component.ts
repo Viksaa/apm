3f4000cec8474873c3e7f47760e1f1a12b45773a
Viktor Stojkoski <viktor.98@hotmail.com>
2019-06-16 19:47:39 +0200

    Updated the Product Detail Page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 
 @Component({
@@ -11,23 +12,24 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
+    errorMessage: string;
 
-    constructor(private route: ActivatedRoute, private router: Router) { }
+    constructor(private route: ActivatedRoute, private router: Router, private productService: ProductService) { }
 
     ngOnInit() {
         // the plus is javascript shortcut to convert the parametar to number
-        let id = +this.route.snapshot.paramMap.get('id');
-        this.pageTitle += `: ${id}`;
-        this.product = {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        let par = this.route.snapshot.paramMap.get('id');
+        if (par) {
+            let id = +par;
+            this.getProduct(id);
         }
+    }
+
+    getProduct(id: number) {
+        this.productService.getProduct(id).subscribe(
+            product => this.product = product,
+            error => this.errorMessage = <any>error
+        )
     }
 
     onBack(): void {
