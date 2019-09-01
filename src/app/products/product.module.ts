560cf65559bb401ee22803db70f0d165f2a579b0
Viktor Stojkoski <viktor.98@hotmail.com>
2019-09-01 22:04:30 +0200

    Created product edit component and added a route for it

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -7,12 +7,14 @@ import { ProductDetailGuard } from './product-detail.guard';
 import { SharedModule } from '../shared/shared.module';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './product-data';
+import { ProductEditComponent } from './product-edit.component';
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
         ConvertToSpacesPipe,
+        ProductEditComponent,
     ],
     imports: [
         RouterModule.forChild([
@@ -21,6 +23,10 @@ import { ProductData } from './product-data';
                 path: 'products/:id',
                 canActivate: [ ProductDetailGuard ],
                 component: ProductDetailComponent
+            },
+            {
+                path: 'products/edit/:id',
+                component: ProductEditComponent
             }
         ]),
         SharedModule,
